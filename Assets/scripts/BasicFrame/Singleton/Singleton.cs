bd6fb1e1917793517c0d7ba5fbec34c4930b2a4e
Jason Gu <1057337859@qq.com>
2022-02-17 16:46:39 -0800

    单例模式基类

---
+++ Assets/scripts/BasicFrame/Singleton/Singleton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#region 单例模式基类
+//用于减少单例模式代码的书写
+//继承了该类，即可变为单例类 使用 类名.Instance.方法 调用其中方法
+//----继承了该类之后不能使用MonoBehavior中方法----
+//----该单例模式为懒汉单例，不考虑多线程情况----
+#endregion
+
+public class Singleton<T> where T:new()
+{
+    private static T instance;
+    public static T Instance
+    {
+        get
+        {
+        if (instance == null)
+                instance = new T();
+        return instance;
+        }
+    }
+}
+
