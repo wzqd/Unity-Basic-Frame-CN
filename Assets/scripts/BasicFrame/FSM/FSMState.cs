1b26d3041528c63fd8119dc563ace046c6690040
Jason Gu <1057337859@qq.com>
2022-03-20 13:11:32 -0700

    fsm 更改 暂时舍弃带transition的设计

--- Assets/scripts/BasicFrame/FSM/FSMState.cs
+++ Assets/scripts/BasicFrame/FSM/FSMState.cs
@@ -1,36 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using Newtonsoft.Json;
-using UnityEditor;
 
 
-namespace BasicFrame.FSM
+public abstract class FSMState
 {
-    public abstract class FSMState
+    public Enum StateType;
+    private FSMSystem FSM;
+    public virtual void Init(FSMSystem fsm, Enum stateType)
     {
-        public e_State State { get; set; }
-        private Dictionary<e_Transition, e_State> transStateDict;
-        private List<FSMTransition> transitionList;
-        public FSMState()
-        {
-            transStateDict = new Dictionary<e_Transition, e_State>();
-            transitionList = new List<FSMTransition>();
-            Construct();
-        }
-
-        protected abstract void Construct();
-
-        public void AddToTransState(e_State state, e_Transition transition)
-        {
-            transStateDict.Add(transition, state);
-            
-            Type newTransType = Type.GetType(transition + "Trans");
-            FSMTransition newTrans = Activator.CreateInstance(newTransType) as FSMTransition;
-            transitionList.Add(newTrans);
-        }
-        
-        public virtual void Enter(){}
-        public virtual void Act(){}
-        public virtual void Exit(){}
+        this.FSM = fsm;
+        this.StateType = stateType;
     }
+
+
+    public virtual void Enter(){}
+    public virtual void Act(){}
+    public virtual void Exit(){}
 }
--- Assets/scripts/BasicFrame/FSM/e_Transition.cs
+++
@@ -1,4 +0,0 @@
-﻿public enum e_Transition
-{
-    
-}
