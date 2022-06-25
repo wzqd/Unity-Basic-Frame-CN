fc8ee18d161f1e8347a0007f09562f3343e14d69
Jason Gu <1057337859@qq.com>
2022-06-25 19:08:35 -0400

    优化并完成了状态和状态机的基类，完成有限状态机的系统

---
+++ Assets/scripts/BasicFrame/FSM/BaseFSM.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#region 状态机基类
+//有限状态机的状态机基类
+
+//其中主要的方法是修改状态，需要的话可以设置初始状态
+//Update函数可以覆写，在其中加入何时修改状态的逻辑
+//使用时需要准备一个枚举，********枚举选项名必须和状态名相同，因为要用反射创建状态对象********
+
+//优化思路可以用整型代替枚举，避免装拆箱子    可以加入对象池系统
+#endregion
+public abstract class BaseFSM: MonoBehaviour
+{
+    private List<BaseState> stateList = new List<BaseState>();//后续可以用对象池替换（小体量状态不是非常需要）
+    public Enum CurrentStateType { get; set; } //枚举类型,可以设置一个初始状态
+    private BaseState currentState;
+
+
+    /// <summary>
+    /// 虚方法 生命周期函数 使用时记得加override关键字
+    /// </summary>
+    protected virtual void Update()
+    {
+        if (currentState != null)
+        {
+            currentState.Act(); //每一帧执行状态内的方法
+        }
+    }
+
+    /// <summary>
+    ///修改状态
+    /// </summary>
+    /// <param name="newState"></param>
+    public void ChangeState(Enum newState)
+    {
+        if (newState == CurrentStateType) return; //如果重复进入同一个状态，直接返回
+        if(currentState != null) currentState.Exit(); //执行退出当前状态的方法
+
+        currentState = GetStateObj(newState); //得到新的状态
+        currentState.Enter(); //执行新状态的进入方法
+        
+
+    }
+
+    /// <summary>
+    ///从列表中得状态
+    /// </summary>
+    /// <param name="stateType"></param>
+    /// <returns></returns>
+    private BaseState GetStateObj(Enum stateType) //此处有装拆箱问题，以后解决
+    {
+        foreach (var stateObj in stateList) //如果状态已经存在列表内，直接返回
+        {
+            if (stateObj.StateType == stateType) return stateObj;
+        }
+
+        //*********枚举中的类型名要和状态类名一直，否则会创建空物体*********
+        BaseState state = Activator.CreateInstance(Type.GetType(stateType.ToString())!) as BaseState; //若不存在，创建一个
+        state.Init(this,stateType); //将其初始化为传入的枚举类型
+        stateList.Add(state); //加入列表
+        return state;
+    }
+}
---
+++ Assets/scripts/BasicFrame/FSM/BaseState.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+#region 状态基类
+//有限状态机中的状态基类
+
+//其中主要的方法为三个虚方法，分别在进入，退出和持续状态时调用
+//只需继承该类并覆写需要的方法即可
+//********注意继承的类必须和对应状态机的枚举选项名相同，因为是用反射创建的********
+#endregion
+public abstract class BaseState
+{
+    public Enum StateType;
+    private BaseFSM baseFSM;
+    public virtual void Init(BaseFSM baseFsm, Enum stateType)
+    {
+        this.baseFSM = baseFsm; //归属的状态机
+        this.StateType = stateType; //初始的状态
+    }
+
+    /// <summary>
+    /// 进入状态时干什么
+    /// </summary>
+    public virtual void Enter(){} 
+    /// <summary>
+    /// 在状态中每一帧干什么
+    /// </summary>
+    public virtual void Act(){} 
+    /// <summary>
+    /// 退出状态时干什么
+    /// </summary>
+    public virtual void Exit(){}
+}
